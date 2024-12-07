e6dd0fc85a713579dcdd0e6e98ba135d286eb136
Andrzej <andrzej@rpi.pl>
2024-12-07 14:43:28 +0100

    kod done

--- pd2/solve.r
+++ pd2/solve.r
@@ -1,18 +1,11 @@
+options("width"=100)
+
 data_file <- paste(getwd(), "/dane/DaneR.csv", sep="")
 dane <- read.csv(data_file, header=TRUE, sep=";", dec=",", encoding="UTF-8")
 
 
-# ----------------------------------------------
-
-# podpunkt a
-# proszę wybrać dane dla jednego roku (np. do nowego obiektu), a następnie proszę dla
-# każdego województwa przygotować podstawowe podsumowania wybranej zmiennej
-# (liczbowej) – proszę użyć odpowiedniej funkcji, tak żeby podział na województwa był
-# automatyczny; w tym punkcie można napisać kilka słów komentarza;
-
+# Podpunkt a:
 # wybrany rok 2012
-
-# lista_wojewodztw <- unique(dane2012$Wojewodztwo)
 
 dane2012 <- dane[dane$Rok == 2012,]
 by(dane2012$XA20, dane2012$Wojewodztwo, summary)
@@ -20,38 +13,15 @@ by(dane2012$XA20, dane2012$Wojewodztwo, summary)
 # TODO: kilka słów komentarze
 
 
-# ----------------------------------------------
-# print("")
-
-# podpunkt b
+# Podpunkt b:
 # proszę przygotować odpowiednią tabelę pokazującą liczbę powiatów w kontekście
 # nakładów inwestycyjnych w przedsiębiorstwach na jednego mieszkańca w
 # poszczególnych latach (wskazówka: jedna z danych ma charakter ciągły – sposób
 # poradzenia sobie z tym problemem należy odszukać w przykładach z materiałów);
 
-# filter NAs:
-# dane_no_na$XA17
-# cut(dane$XA17, breaks=c(1, 2, 1000))
-# cut(dane_no_na$XA17, fivenum(dane_no_na$XA17), dig.lab=6 )
-
-# dane_no_na_1 = dane[!is.na(dane$XA17),]
-# dane_no_na = dane_no_na_1[!is.na(dane_no_na_1$XA29),]
-
-# dane_no_na$XA17_int = as.double(dane_no_na$XA17)
-
-dane$naklad_na_mieszkanca <- dane$XA17 / dane$XA29
-
-# cut(dane$naklad_na_mieszkanca, breaks=((0:10)*50))
-
-
-table <- table(dane$Rok, cut(dane$naklad_na_mieszkanca, breaks=5))
-table
-
-
-
-
-# ----------------------------------------------
-
+dane$naklad_na_mieszkanca <- (dane$XA17 * 1e6) / dane$XA29
+tabela <- table(cut(dane$naklad_na_mieszkanca, breaks=10), dane$Rok)
+tabela
 
 
 
@@ -59,13 +29,21 @@ table
 # proszę przygotować stworzoną w poprzednim punkcie tabelę w wartościach
 # procentowych – jaki wymiar powinien być bazą 100%?
 
+# Wymiar 100% -- sumaryczna liczba województw
+# lata <- rownames(table)
+# lata
+
+round(prop.table(tabela,2),2) 
+
 
 # ----------------------------------------------
 
 # podpunkt d
-# proszę sprawdzić (dodając odpowiednie podsumowanie do tabeli z p. b), czy w każdym
-# roku jest taka sama liczba powiatów.
 
 
-
-# ----------------------------------------------
+liczba_lat <- ncol(tabela)
+liczba_w = replicate(liczba_lat, 0)
+for (i in 1:liczba_lat) {
+    liczba_w[i] <- sum(tabela[,i])
+}
+liczba_w
