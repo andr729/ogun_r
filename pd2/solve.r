6fc57fca2925727f5d585605863bb1b63ef0b2d0
Andrzej <andrzej@rpi.pl>
2024-12-07 14:45:36 +0100

    format

--- pd2/solve.r
+++ pd2/solve.r
@@ -3,43 +3,38 @@ options("width"=100)
 data_file <- paste(getwd(), "/dane/DaneR.csv", sep="")
 dane <- read.csv(data_file, header=TRUE, sep=";", dec=",", encoding="UTF-8")
 
+# # # # # # # # #
+#  Podpunkt a:  #
+# # # # # # # # #
 
-# Podpunkt a:
 # wybrany rok 2012
 
 dane2012 <- dane[dane$Rok == 2012,]
 by(dane2012$XA20, dane2012$Wojewodztwo, summary)
 
-# TODO: kilka słów komentarze
 
 
-# Podpunkt b:
-# proszę przygotować odpowiednią tabelę pokazującą liczbę powiatów w kontekście
-# nakładów inwestycyjnych w przedsiębiorstwach na jednego mieszkańca w
-# poszczególnych latach (wskazówka: jedna z danych ma charakter ciągły – sposób
-# poradzenia sobie z tym problemem należy odszukać w przykładach z materiałów);
+# # # # # # # # #
+#  Podpunkt b:  #
+# # # # # # # # #
+
 
 dane$naklad_na_mieszkanca <- (dane$XA17 * 1e6) / dane$XA29
 tabela <- table(cut(dane$naklad_na_mieszkanca, breaks=10), dane$Rok)
 tabela
 
 
-
-# podpunkt c
-# proszę przygotować stworzoną w poprzednim punkcie tabelę w wartościach
-# procentowych – jaki wymiar powinien być bazą 100%?
+# # # # # # # # #
+#  Podpunkt c:  #
+# # # # # # # # #
 
 # Wymiar 100% -- sumaryczna liczba województw
-# lata <- rownames(table)
-# lata
-
 round(prop.table(tabela,2),2) 
 
 
-# ----------------------------------------------
-
-# podpunkt d
-
+# # # # # # # # #
+#  Podpunkt d:  #
+# # # # # # # # #
 
 liczba_lat <- ncol(tabela)
 liczba_w = replicate(liczba_lat, 0)
