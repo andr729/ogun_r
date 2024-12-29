1cb2cb12204cee3ee8b60c91bb05b1df59145175
Andrzej <andrzej@rpi.pl>
2024-12-29 17:38:25 +0100

    notatki

--- pd3/solve.r
+++ pd3/solve.r
@@ -3,6 +3,11 @@ options("width"=100)
 data_file <- paste(getwd(), "/dane/DaneR.csv", sep="")
 dane <- read.csv(data_file, header=TRUE, sep=";", dec=",", encoding="UTF-8")
 
+# @TODO: us to
+# dane.sorted <- dane[order(dane$Powiat, dane$Rok),]
+# dane.sorted$Powiat[dane.sorted$Rok==2015]
+# nlevels(factor(dane$Powiat[dane$Rok==2015]))
+
 # XB03 -- Liczba dzieci na 1 miejsce w przedszkolu
 
 # Porównanie lat 2015 i 2014:
@@ -22,11 +27,22 @@ t.test(XB03.2015, XB03.2014, paired=TRUE, var.equal=TRUE)
 # avg:
 dane_przed_2015 <- dane[dane$Rok<2015,]
 
-XB03.avg <- as.vector(tapply(dane_przed_2015$XB03, dane_przed_2015$Powiat, mean))
+# @TODO: napisać, czemu po kod_powiat
+# XB03.avg <- as.vector(tapply(dane_przed_2015$XB03, dane_przed_2015$Kod_powiat, mean))
+
+XB03.avg <- as.vector(aggregate(dane_przed_2015$XB03, by=list(dane_przed_2015$Kod_powiat), FUN=mean, na.rm=TRUE))
+
+# length(dane$XB03[dane$Rok==2015])
+# length(XB03.2015)
+# length(XB03.avg)
 
 summary(XB03.avg)
-var.test(XB03.2015, XB03.avg)
-t.test(XB03.2015, XB03.avg)
+# var.test(XB03.2015, XB03.avg)
+# t.test(XB03.2015, XB03.avg, paired=TRUE)
 
 
+
+
+
+# Powiat nowodworski duplikat :o
 
