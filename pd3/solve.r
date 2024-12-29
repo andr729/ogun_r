33d6ae7f36fdf7b38bd9eb613a66c0ca371b17d5
Andrzej <andrzej@rpi.pl>
2024-12-29 17:49:51 +0100

    notki

--- pd3/solve.r
+++ pd3/solve.r
@@ -3,11 +3,6 @@ options("width"=100)
 data_file <- paste(getwd(), "/dane/DaneR.csv", sep="")
 dane <- read.csv(data_file, header=TRUE, sep=";", dec=",", encoding="UTF-8")
 
-# @TODO: us to
-# dane.sorted <- dane[order(dane$Powiat, dane$Rok),]
-# dane.sorted$Powiat[dane.sorted$Rok==2015]
-# nlevels(factor(dane$Powiat[dane$Rok==2015]))
-
 # XB03 -- Liczba dzieci na 1 miejsce w przedszkolu
 
 # Porównanie lat 2015 i 2014:
@@ -19,29 +14,23 @@ summary(XB03.2015)
 summary(XB03.2014)
 
 var.test(XB03.2015, XB03.2014)
+
+# Do raportu: p-value wyszło wysokie, więc nie odrzucamy tego, że wariancja jest różna
+
 t.test(XB03.2015, XB03.2014, paired=TRUE, var.equal=TRUE)
 
+# Do raportu: p-value wyszło małe, więc średnie są te same
+
 # Porównanie lat 2015 i średniej za wszystkie poprzednie lata:
-
-
-# avg:
 dane_przed_2015 <- dane[dane$Rok<2015,]
-
-# @TODO: napisać, czemu po kod_powiat (albo nie)
-# XB03.avg <- as.vector(tapply(dane_przed_2015$XB03, dane_przed_2015$Kod_powiat, mean))
 
 XB03.avg <- as.vector(aggregate(dane_przed_2015$XB03, by=list(dane_przed_2015$Kod_powiat), FUN=mean, na.rm=TRUE)$x)
 
 summary(XB03.avg)
 var.test(XB03.2015, XB03.avg)
-t.test(XB03.2015, XB03.avg, paired=TRUE)
+# Do raportu: p-value wyszło małe, wariancja może być różna, więc nie przekazujemy var.equal=TRUE
 
 
+t.test(XB03.2015, XB03.avg, paired=TRUE, var.equal=FALSE)
+# Do raportu: p-value wyszło małe, więc średnie są różne, odrzucamy H0, średnie się różnią
 
-
-
-# Powiat nowodworski duplikat :o
-
-
-
-warnings()
