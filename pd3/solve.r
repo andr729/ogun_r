0527bcd983cfc1d6987b0f79ef026fe8a1fcd4f4
Andrzej <andrzej@rpi.pl>
2024-12-29 17:19:35 +0100

    error fix

--- pd3/solve.r
+++ pd3/solve.r
@@ -3,32 +3,30 @@ options("width"=100)
 data_file <- paste(getwd(), "/dane/DaneR.csv", sep="")
 dane <- read.csv(data_file, header=TRUE, sep=";", dec=",", encoding="UTF-8")
 
-# XB23 -- miejsca w przedszkolach
-# XB24 -- dzieci w wieku 3-6 lat
-
-dane$dzieci_na_miejsce <- dane$XB24 / dane$XB23
+# XB03 -- Liczba dzieci na 1 miejsce w przedszkolu
 
 # Porównanie lat 2015 i 2014:
 
-dzieci_na_miejsce.2015<-as.vector(dane$dzieci_na_miejsce[dane$Rok==2015])
-dzieci_na_miejsce.2014<-as.vector(dane$dzieci_na_miejsce[dane$Rok==2014])
+XB03.2015<-as.vector(dane$XB03[dane$Rok==2015])
+XB03.2014<-as.vector(dane$XB03[dane$Rok==2014])
 
-summary(dzieci_na_miejsce.2015)
-summary(dzieci_na_miejsce.2014)
+summary(XB03.2015)
+summary(XB03.2014)
 
-var.test(dzieci_na_miejsce.2015, dzieci_na_miejsce.2014)
-t.test(dzieci_na_miejsce.2015, dzieci_na_miejsce.2014, paired=TRUE, var.equal=TRUE)
+var.test(XB03.2015, XB03.2014)
+t.test(XB03.2015, XB03.2014, paired=TRUE, var.equal=TRUE)
 
 # Porównanie lat 2015 i średniej za wszystkie poprzednie lata:
 
+
+# avg:
 dane_przed_2015 <- dane[dane$Rok<2015,]
-dzieci_na_miejsce.avg <- as.vector(tapply(dane_przed_2015$dzieci_na_miejsce, dane_przed_2015$Powiat, mean))
 
-summary(dzieci_na_miejsce.avg)
-var.test(dzieci_na_miejsce.2015, dzieci_na_miejsce.avg)
+XB03.avg <- as.vector(tapply(dane_przed_2015$XB03, dane_przed_2015$Powiat, mean))
 
-# paired nie działa, bo są różnej długości... (pytanie czemu?)
-t.test(dzieci_na_miejsce.2015, dzieci_na_miejsce.avg, var.equal=TRUE)
+summary(XB03.avg)
+var.test(XB03.2015, XB03.avg)
+t.test(XB03.2015, XB03.avg)
 
 
 
